1b19979d945e335841c407bb2f6f99f10e52cd0c
Bernard Budano <bbudano@gmail.com>
2022-12-07 18:20:25 +0100

    Implemented BatchMapping endpoint to prevent n+1 problem when fetching customer orders

--- customer-service/src/main/java/hr/bbudano/customerservice/customer/controller/CustomerController.java
+++ customer-service/src/main/java/hr/bbudano/customerservice/customer/controller/CustomerController.java
@@ -4,13 +4,14 @@ import hr.bbudano.customerservice.customer.entity.Customer;
 import hr.bbudano.customerservice.customer.service.CustomerService;
 import hr.bbudano.customerservice.dto.OrderDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.MutationMapping;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.graphql.data.method.annotation.*;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -48,9 +49,27 @@ public class CustomerController {
         return customerService.deleteCustomer(id);
     }
 
-    @SchemaMapping(typeName = "Customer")
-    Flux<OrderDto> orders(Customer customer) {
-        return customerService.getCustomerOrders(customer);
+//    @SchemaMapping(typeName = "Customer")
+//    Flux<OrderDto> orders(Customer customer) {
+//        return customerService.getCustomerOrders(customer);
+//    }
+
+    @BatchMapping
+    Mono<Map<Customer, List<OrderDto>>> orders(List<Customer> customers) {
+        var customersOrders = Flux.fromStream(customers
+                .stream()
+                .map(customerService::getCustomerOrders))
+                .flatMap(orders -> orders);
+
+        return customersOrders.collectList()
+                .map(orders -> {
+                    Map<Integer, List<OrderDto>> collect = orders.stream()
+                            .collect(Collectors.groupingBy(OrderDto::customerId));
+
+                    return customers.stream()
+                            .collect(Collectors.toMap(customer -> customer,
+                                    customer -> collect.get(customer.getId())));
+                });
     }
 
 }
